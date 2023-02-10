eb62df141dc23856011d5f4220a6b72a7b207194
NoahNym <noahnym@gmail.com>
2023-02-10 09:40:26 +0200

    nu funkar e

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function loadJSON(path, success, error)
-{
+function loadJSON(path, success, error) {
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 if (success)
@@ -18,10 +16,38 @@ function loadJSON(path, success, error)
 }
 
 
-loadJSON('./energy.json' ,
-        (data)=> {
-            //här fortsätter du
-         console.log(data);
-        },
-        (err)=> { console.error(err); }
+loadJSON('./energy.json',
+    (data) => {
+        //här fortsätter du
+        displayData(data);
+        console.log(data);
+    },
+    (err) => { console.error(err); }
 );
+function displayData(data) {
+
+    let table = document.getElementById("table")
+
+    for (let i = 0; i < data.length; i++) {
+        const tr = document.createElement("tr");
+        const tdf = document.createElement("td");
+        const tdC = document.createElement("td");
+        const tdc = document.createElement("td");
+
+
+        tdC.innerHTML = data[i].country;
+        tdc.innerHTML = data[i].consumption;
+
+        const img = document.createElement("img");
+        img.src = data[i].flag;
+        img.width = "30"
+
+        tdf.appendChild(img);
+
+        table.appendChild(tr);
+        table.appendChild(tdf);
+        table.appendChild(tdC);
+        table.appendChild(tdc);
+    }
+
+};
